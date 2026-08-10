11ce8a0cbba1d16ab40f99252d2dc90a35d44bec
[Kareem Ramadan] <[kramadan@petroamir.com]>
2026-08-10 08:53:23 +0300

    Create Domain,Application,Infrastructure Libraries

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
 
+using HR.Domain.Models.Identity;
+using HR.Infrastructure.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
+
 namespace HRBackEndApi
 {
     public class Program
@@ -9,24 +16,124 @@ namespace HRBackEndApi
 
             // Add services to the container.
 
+            #region AddDataBaseConnection
+
+            //Add Identity DataBase Connection
+            //=================================
+            builder.Services.AddDbContext<IdentityContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityConnection")));
+
+            //Add Application DataBase Connection
+            //=================================
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
+            //=========================================================================
+
+            #endregion
+
+            #region AddIdentityServices
+
+            //Add Identity Services
+            //=========================
+
+            builder.Services.AddIdentity<AppUser, AppRole>()
+                .AddEntityFrameworkStores<IdentityContext>()
+                .AddDefaultTokenProviders()
+                .AddApiEndpoints();
+
+            #endregion
+
+
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+
+            #region SwaggerConfigurationService
+
+            //configure swagger for API documentation
+            //========================================
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "HRMS Web API",
+                    Version = "v1",
+                    Description = "This is a HRMS Web API for managing human resource system",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Kareem Sayed Ramadan",
+                        Email = "kramadan@petroamir.com",
+                    }
+                });
+            });
+
+            #endregion
+
+
+            #region CORSConfigurationService
+
+
+            // Configure CORS to allow requests from any origin
+            //=========================================================
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("*");
+                });
+            });
+
+
+            #endregion
+
+            #region RegisterRepositoriesServices
+
+            //Register Repositoriesand Interfaces Services
+            //=============================================
+
+            //builder.Services.AddTransient(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+
+            #endregion
+
+            #region RegisterMappers
+
+            //builder.Services.AddAutoMapper(m => { }, typeof(CityMappingProfile));
+            //builder.Services.AddAutoMapper(m => { }, typeof(CountryMappingProfile));
+            //builder.Services.AddAutoMapper(m => { }, typeof(CompanyMappingProfile));
+            //builder.Services.AddAutoMapper(m => { }, typeof(GovernorateMappingProfile));
+
+            #endregion
+
 
             var app = builder.Build();
+
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
-            app.UseHttpsRedirection();
+            #region AddMiddleWarePipelines
 
+
+            // Apply the CORS policy
+            //======================
+            app.UseCors("CorsPolicy");
+
+            // Enable HTTPS redirection
+            //=========================
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            // Enable Authentication and Authorization Middleware
+            //===================================================
+            app.UseAuthentication();
             app.UseAuthorization();
 
-
             app.MapControllers();
+
+            #endregion
+
 
             app.Run();
         }
